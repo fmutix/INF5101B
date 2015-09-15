3446996ae93bd5505bd0ad27c1b68b6573edba26
Obside <obsiagae@gmail.com>
2015-09-15 23:35:32 +0200

    UNIQUE constraint on Users' email

--- queries/Users.sql
+++ queries/Users.sql
@@ -7,6 +7,6 @@ CREATE TABLE Users
         (START WITH 1, INCREMENT BY 1),
     FirstName VARCHAR(200),
     LastName VARCHAR(200),
-    Email VARCHAR(200),
+    Email VARCHAR(200) UNIQUE,
     Phone VARCHAR(200)
 )
