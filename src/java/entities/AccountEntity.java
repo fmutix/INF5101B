80e57596c2db165c0658d8833ff8933a4d3d3aa8
Obside <obsiagae@gmail.com>
2015-10-22 23:53:42 +0200

    fix id persistence & fix admin account

--- src/java/entities/AccountEntity.java
+++ src/java/entities/AccountEntity.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 public class AccountEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String firstName;
