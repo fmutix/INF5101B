4e30c77469bb27e90cdaf7688fcabd3331f73ede
Obside <obsiagae@gmail.com>
2015-11-02 03:54:44 +0100

    OrderEntity

--- src/java/controllers/Shop.java
+++ src/java/controllers/Shop.java
@@ -4,22 +4,37 @@ import entities.OrderEntity;
 import javax.faces.bean.ManagedBean;
 import entities.FoodEntity;
 import entities.FoodRepository;
+import entities.OrderRepository;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
 public class Shop {
     
-    private OrderEntity order;
+    @ManagedProperty(value="#{account}")
+    private Account account;
     
 	@EJB private FoodRepository foodRepository;
+    @EJB private OrderRepository orderRepository;
+    
+    private OrderEntity order;
 
 	public Shop() {
+        order = new OrderEntity();
+    }
+    
+    public void submitOrder() {
+        order.setAccount(account.getEntity());
+        orderRepository.persist(order);
         order = new OrderEntity();
     }
 	
     public List<FoodEntity> getFoodList() { return foodRepository.findAll(); }
     public OrderEntity getOrder() { return order; }
+    
+    public void setAccount(Account account) { this.account = account; }
+    
 }
---
+++ src/java/entities/OrderRepository.java
@@ -0,0 +1,37 @@
+package entities;
+
+import java.util.Date;
+import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+@Stateless
+@LocalBean
+public class OrderRepository {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public List<OrderEntity> findAll() {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+        
+        CriteriaQuery<OrderEntity> cq = cb.createQuery(OrderEntity.class);
+        Root<OrderEntity> root = cq.from(OrderEntity.class);
+        cq.select(root);
+        cq.orderBy(cb.asc(root.get("orderTime")));
+        Query q = em.createQuery(cq);
+        
+        return q.getResultList();
+	}
+	
+	public void persist(OrderEntity o) {
+        o.setOrderTime(new Date());
+		em.persist(o);
+	}
+}
